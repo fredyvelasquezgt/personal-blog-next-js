365327b2d75f305c99ed8e617fa091153c33058f
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-15 12:05:45 -0600

    added image to hero component

---
+++ components/home-page/hero.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/fredy.jpeg"
+          alt="An image showing Fredy"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am Fredy</h1>
+      <p>I blog about software development</p>
+    </section>
+  );
+}
+
+export default Hero;

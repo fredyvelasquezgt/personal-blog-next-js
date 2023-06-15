001fe699b1d872e3fe78c30610c5dc60b1c1f263
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-15 16:11:02 -0600

    created dummy data

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,17 @@ import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from '../components/home-page/hero'
 
+const DUMMY_POSTS = [
+  {
+    slug: '', title, image, excerpt, date
+  }
+]
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={} />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }

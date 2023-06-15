3e9b5dad045218c518cbeb576f4c1c203afbb4a5
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-15 16:12:21 -0600

    filling dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
-import Hero from '../components/home-page/hero'
+import Hero from "../components/home-page/hero";
 
 const DUMMY_POSTS = [
   {
-    slug: '', title, image, excerpt, date
-  }
-]
+    slug: "getting-started-with-next-js",
+    title: "Getting Started with Next JS",
+    image: "getting-started-nextjs.png",
+    excerpt: "learn easy nextJS",
+    date: '2023-06-15',
+  },
+];
 
 function HomePage() {
   return (

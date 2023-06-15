90c30fdad7beffcaf06c533e594a4e73103e70fc
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-15 16:08:09 -0600

    rendering PostGrid component

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,8 +1,10 @@
+import PostsGrid from '../posts/posts-grid'
 import classes from './featured-posts.module.css'
 
 function FeaturedPosts() {
     return <section className={classes.latest} >
         <h2>Featured Posts</h2>
+        <PostsGrid />
     </section>
 }
 

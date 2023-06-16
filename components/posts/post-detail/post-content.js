db694d6f17a66437655002fc39ba5876d82f8784
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-16 10:44:37 -0600

    turning markdown into jsx

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,24 @@
+import ReactMarkdown from 'react-markdown';
+import PostHeader from './post-header';
+import classes from './post-content.module.css'
+
+const DUMMY_POST = {
+    slug: "getting-started-with-next-js",
+    title: "Getting Started with Next JS",
+    image: "getting-started-nextjs.png",
+    date: '2023-06-15',
+    content: '# This is a first post'
+  }
+
+  function PostContent() {
+
+    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+
+    return <article className={classes.content}>
+        <PostHeader title={DUMMY_POST.title} image={imagePath} />
+        <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+    </article>
+
+}
+
+export default PostContent;

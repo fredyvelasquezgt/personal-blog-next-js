eb36c4a801c93cbc48d7e69b4a673cffff8e9ccc
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-16 10:16:35 -0600

    new changes

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,41 @@
-function AllPostsPage() {
 
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+    {
+      slug: "getting-started-with-next-js",
+      title: "Getting Started with Next JS",
+      image: "getting-started-nextjs.png",
+      excerpt: "learn easy nextJS",
+      date: '2023-06-15',
+    },
+    {
+      slug: "getting-started-with-next-js2",
+      title: "Getting Started with Next JS",
+      image: "getting-started-nextjs.png",
+      excerpt: "learn easy nextJS",
+      date: '2023-06-15',
+    },
+    {
+      slug: "getting-started-with-next-js3",
+      title: "Getting Started with Next JS",
+      image: "getting-started-nextjs.png",
+      excerpt: "learn easy nextJS",
+      date: '2023-06-15',
+    },
+    {
+      slug: "getting-started-with-next-js4",
+      title: "Getting Started with Next JS",
+      image: "getting-started-nextjs.png",
+      excerpt: "learn easy nextJS",
+      date: '2023-06-15',
+    }
+  
+  ];
+  
+
+function AllPostsPage() {
+    return <AllPosts posts={DUMMY_POSTS} />
 }
 
 export default AllPostsPage;

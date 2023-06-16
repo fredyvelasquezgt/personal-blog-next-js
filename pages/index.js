7a07df97fd64edb39e08ddb37cd872c4e657ba13
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-16 11:12:21 -0600

    markdown as source information

--- pages/index.js
+++ pages/index.js
@@ -2,39 +2,10 @@ import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-next-js",
-    title: "Getting Started with Next JS",
-    image: "getting-started-nextjs.png",
-    excerpt: "learn easy nextJS",
-    date: '2023-06-15',
-  },
-  {
-    slug: "getting-started-with-next-js2",
-    title: "Getting Started with Next JS",
-    image: "getting-started-nextjs.png",
-    excerpt: "learn easy nextJS",
-    date: '2023-06-15',
-  },
-  {
-    slug: "getting-started-with-next-js3",
-    title: "Getting Started with Next JS",
-    image: "getting-started-nextjs.png",
-    excerpt: "learn easy nextJS",
-    date: '2023-06-15',
-  },
-  {
-    slug: "getting-started-with-next-js4",
-    title: "Getting Started with Next JS",
-    image: "getting-started-nextjs.png",
-    excerpt: "learn easy nextJS",
-    date: '2023-06-15',
-  }
 
-];
 
 function HomePage() {
+  
   return (
     <Fragment>
       <Hero />

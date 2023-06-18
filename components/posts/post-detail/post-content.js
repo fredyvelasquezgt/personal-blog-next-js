97cecce4130220683e227ca35ea80e4d65a66257
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-17 23:47:35 -0600

    added comments

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -10,9 +10,11 @@ const DUMMY_POST = {
     content: '# This is a first post'
   }
 
+  //try new images
+
   function PostContent() {
 
-    const imagePath = `public/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
 
     return <article className={classes.content}>
         <PostHeader title={DUMMY_POST.title} image={imagePath} />

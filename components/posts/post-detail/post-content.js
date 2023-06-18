4df7333b9b9647aeef5f99883af91c45bb1fdc72
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-18 16:19:36 -0600

    fixed markwdown render

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,38 +3,41 @@ import Image from 'next/image'
 import PostHeader from './post-header';
 import classes from './post-content.module.css'
 
-
-
   //try new images
 
   function PostContent(props) {
-
-    const {post} = props;
-
-    const imagePath = `/images/posts/${post.slug}/${post.image}`
-
-    const customRenderer = {
-    
-
-      paragraph(paragraph) {
-        const {node} = paragraph;
-
-        if(node.children[0].type === 'image') {
+    const { post } = props;
+    const imagePath = `/images/posts/${post.slug}/${post.image}`;
+   
+    const customRenderers = {
+      p: ({ node, ...props }) => {
+        console.log(node.children[0]);
+        if (node.children[0].tagName && node.children[0].tagName === 'img') {
           const image = node.children[0];
-
-          return <div className={classes.image} >
-                <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
-
-          </div>
+          return (
+            <div className={classes.image}>
+              {
+                <Image
+                  src={`/images/posts/getting-started-with-nextjs/${image.properties.src}`}
+                  alt={image.properties.alta}
+                  width={600}
+                  height={300}
+                />
+              }
+            </div>
+          );
         }
-      }
+        return <p>{props.children}</p>;
+      },
     };
-
-    return <article className={classes.content}>
+   
+    return (
+      <article className={classes.content}>
         <PostHeader title={post.title} image={imagePath} />
-        <ReactMarkdown components={customRenderer}>{post.content}</ReactMarkdown>
-    </article>
-
-}
+        <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
+      </article>
+    );
+  }
+  
 
 export default PostContent;

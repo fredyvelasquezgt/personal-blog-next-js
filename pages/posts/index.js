98bd9d764ef3b1418b6447cbb45f7f8627eda255
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-18 12:53:11 -0600

    static props for all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,41 +1,15 @@
 
-import AllPosts from '../../components/posts/all-posts';
-
-const DUMMY_POSTS = [
-    {
-      slug: "getting-started-with-next-js",
-      title: "Getting Started with Next JS",
-      image: "getting-started-nextjs.png",
-      excerpt: "learn easy nextJS",
-      date: '2023-06-15',
-    },
-    {
-      slug: "getting-started-with-next-js2",
-      title: "Getting Started with Next JS",
-      image: "getting-started-nextjs.png",
-      excerpt: "learn easy nextJS",
-      date: '2023-06-15',
-    },
-    {
-      slug: "getting-started-with-next-js3",
-      title: "Getting Started with Next JS",
-      image: "getting-started-nextjs.png",
-      excerpt: "learn easy nextJS",
-      date: '2023-06-15',
-    },
-    {
-      slug: "getting-started-with-next-js4",
-      title: "Getting Started with Next JS",
-      image: "getting-started-nextjs.png",
-      excerpt: "learn easy nextJS",
-      date: '2023-06-15',
-    }
-  
-  ];
+import AllPosts from '../../components/posts/all-posts';   
+import { getStaticProps } from '../index';
+    
   
 
 function AllPostsPage() {
     return <AllPosts posts={DUMMY_POSTS} />
+}
+
+export function getStaticProps() {
+  
 }
 
 export default AllPostsPage;
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -1,6 +1,6 @@
 ---
 title: 'Getting started with NextJS'
-date: '2023-06-11'
+date: '2023-06-8'
 image: getting-started-nextjs.png
 excerpt: learning next JSS
 isFeatured: true

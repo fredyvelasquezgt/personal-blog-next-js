c1cb2e3454e6d4638b5a00b7acd3180a75f151b1
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-18 16:12:02 -0600

    added render image with Image

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image'
 import PostHeader from './post-header';
 import classes from './post-content.module.css'
 
@@ -12,9 +13,26 @@ import classes from './post-content.module.css'
 
     const imagePath = `/images/posts/${post.slug}/${post.image}`
 
+    const customRenderer = {
+    
+
+      paragraph(paragraph) {
+        const {node} = paragraph;
+
+        if(node.children[0].type === 'image') {
+          const image = node.children[0];
+
+          return <div className={classes.image} >
+                <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
+
+          </div>
+        }
+      }
+    };
+
     return <article className={classes.content}>
         <PostHeader title={post.title} image={imagePath} />
-        <ReactMarkdown>{post.content}</ReactMarkdown>
+        <ReactMarkdown components={customRenderer}>{post.content}</ReactMarkdown>
     </article>
 
 }

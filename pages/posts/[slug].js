eeaf5ca91f9f96f541d7ce4e800e7f400a2cb296
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-18 13:05:04 -0600

    prep for pre render

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,29 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData } from "../../lib/posts-util";
 
 function PostDetailPage() {
     return <PostContent />
+}
+
+export function getStaticProps(context) {
+    const {params} = context;
+    const {slug} = params;
+
+    const postData = getPostData(slug);
+
+    return {
+        props: {
+            post: postData
+        },
+        revalidate: 600
+    }
+}
+
+export function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: false
+    }
 }
 
 export default PostDetailPage;

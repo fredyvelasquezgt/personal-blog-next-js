0711bd436a6410c407346452fa16654e93d5a58c
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-18 12:55:17 -0600

    rendering all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,15 +1,21 @@
 
 import AllPosts from '../../components/posts/all-posts';   
-import { getStaticProps } from '../index';
+import { getAllPosts } from '../../lib/posts-util';
     
   
 
-function AllPostsPage() {
-    return <AllPosts posts={DUMMY_POSTS} />
+function AllPostsPage(props) {
+    return <AllPosts posts={props.posts} />
 }
 
 export function getStaticProps() {
-  
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts
+    }
+  }
 }
 
 export default AllPostsPage;

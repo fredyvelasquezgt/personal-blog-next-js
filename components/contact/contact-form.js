22cf8fdadfe19099ab1049dfb0979ee0ebba70b1
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-18 22:34:15 -0600

    added form structure

---
+++ components/contact/contact-form.js
@@ -0,0 +1,18 @@
+import classes from './contact-form.module.css'
+
+function ConctactForm() {
+    return <section className={classes.contact} >
+        <h1>How can I help you?</h1>
+        <form>
+            <div>
+                <div>
+                    <label>
+                        <input />
+                    </label>
+                </div>
+            </div>
+        </form>
+    </section>
+}
+
+export default ConctactForm;

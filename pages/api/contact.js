3b6f5cd77d67321003702392e7e6e2bba10b228f
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-18 22:50:20 -0600

    backend validations

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,10 +1,19 @@
 function handler(req, res) {
-    if(req.method === 'POST') {
-        const {email, name, message} = req.body;
-    
-        
-        
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+        res.status(422).json({message: 'invalid input'})
+        return;
     }
+  }
 }
 
 export default handler;

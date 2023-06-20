b32a3d91cc9ebf6dcb22d22aac20aa615e303382
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-20 11:15:19 -0600

    stored values db

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
 import {MongoClient} from 'mongodb'
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -22,7 +22,27 @@ function handler(req, res) {
         message
     };
 
-    MongoClient.connect('mongodb+srv://<username>:<password>@cluster0.domp7.mongodb.net/?retryWrites=true&w=majority')
+    let client;
+
+    try {
+     client = await MongoClient.connect('mongodb+srv://FredyVelasquez:SanIsidro31.@cluster0.domp7.mongodb.net/my-site?retryWrites=true&w=majority')
+
+    }  catch (error) {
+        res.status(500).json({message: 'could not connect to db'})
+        return;
+    }
+
+    const db = client.db();
+
+    try {
+        const result = await db.collection('messages').insertOne(newMessage);
+        newMessage.id = result.insertedId;
+    } catch (error) {
+        client.close()
+        res.status(500).json({message: 'storing message failes'})
+    }
+
+    client.close();
 
     res
         .status(201)

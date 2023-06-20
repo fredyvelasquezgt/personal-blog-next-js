122f7260a65ce2dc8e042f2ae406ed66561fcdd5
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-20 15:24:31 -0600

    added metadata

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
 import ConctactForm from "../components/contact/contact-form";
+import Head from 'next/head'
 
 function ContactPage() {
-    return <ConctactForm />
+
+    return (
+        <Fragment>
+        <ConctactForm />
+            
+        </Fragment>
+    )
+ 
 }
 
 export default ContactPage;

4e549d13966cdf5b2e574fd6fb48e159688f3532
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-22 15:48:42 -0600

    added dynamic connection

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -26,7 +26,7 @@ async function handler(req, res) {
     const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.domp7.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`
 
     try {
-     client = await MongoClient.connect('mongodb+srv://FredyVelasquez:SanIsidro31.@cluster0.domp7.mongodb.net/my-site?retryWrites=true&w=majority')
+     client = await MongoClient.connect(connectionString)
 
     }  catch (error) {
         res.status(500).json({message: 'could not connect to db'})

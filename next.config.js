9178a7ec0f74d19d6f80a4c9e81aceb9c276ebe4
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-22 16:12:55 -0600

    dev phases added

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    mongodb_username: 'FredyVelasquez',
-    mongodb_password: 'SanIsidro31.',
-    mongodb_clustername: 'cluster0',
-    mongodb_database: 'my-site'
+
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants');
+
+const nextConfig = (phase) => {
+
+  if(phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'FredyVelasquez',
+        mongodb_password: 'SanIsidro31.',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-site-dev'
+      }
+    }
   }
+
+  return {
+    reactStrictMode: true,
+    env: {
+      mongodb_username: 'FredyVelasquez',
+      mongodb_password: 'SanIsidro31.',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'my-site'
+    }
+   
+  }
+  
 }
 
 module.exports = nextConfig
